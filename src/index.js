fa6cdf905cefeb294daf5ceca6972e0f391ee6e4
PRATAP-KUMAR <pratap@fastmail.fm>
2022-08-20 12:36:01 +0530

    COPIED ALL FILES FROM ORIGINAL PROJECT TO THIS PROJECT

---
+++ src/index.js
@@ -0,0 +1,123 @@
+import AddItem from './modules/addItem.js';
+import './style.css';
+import Refresh from './assets/refresh.svg';
+import Enter from './assets/enter.svg';
+
+const box = document.createElement('div');
+box.className = 'box';
+
+const sticky = document.createElement('div');
+sticky.className = 'sticky';
+
+const h2 = document.createElement('h2');
+h2.innerText = "Today's To Do";
+sticky.appendChild(h2);
+
+const refreshBtn = document.createElement('img');
+refreshBtn.setAttribute('src', Refresh);
+refreshBtn.setAttribute('width', 20);
+refreshBtn.setAttribute('height', 20);
+refreshBtn.setAttribute('alt', 'refresh');
+sticky.appendChild(refreshBtn);
+
+//
+box.appendChild(sticky);
+
+const addtolist = document.createElement('div');
+addtolist.className = 'addtolist';
+
+const form = document.createElement('input');
+form.setAttribute('id', 'itemInput');
+form.setAttribute('type', 'input');
+form.setAttribute('name', 'add list item');
+form.setAttribute('placeholder', 'Add list item');
+addtolist.appendChild(form);
+
+const enterButton = document.createElement('button');
+enterButton.setAttribute('id', 'enterButton');
+addtolist.appendChild(enterButton);
+
+const enterBtn = document.createElement('img');
+enterBtn.setAttribute('src', Enter);
+enterBtn.setAttribute('width', 20);
+enterBtn.setAttribute('height', 20);
+enterBtn.setAttribute('alt', 'Enter');
+enterButton.appendChild(enterBtn);
+
+box.appendChild(addtolist);
+
+const content = document.createElement('div');
+content.className = 'content';
+
+box.appendChild(content);
+
+const section = document.body.querySelector('#todo-list');
+section.appendChild(box);
+
+let tasks;
+
+window.onload = () => {
+  if (localStorage.getItem('ITEMS') === null) {
+    tasks = [];
+    return;
+  }
+  tasks = Array.from(JSON.parse(localStorage.getItem('ITEMS')));
+  tasks.forEach((item) => content.appendChild(AddItem(item)));
+};
+
+const addTask = () => {
+  const input = document.getElementById('itemInput');
+  if (input.value !== '') {
+    if (localStorage.getItem('ITEMS') === null) {
+      tasks = [];
+    } else {
+      tasks = Array.from(JSON.parse(localStorage.getItem('ITEMS')));
+    }
+    let length;
+    if (tasks.length === 0) {
+      length = 1;
+    } else {
+      length = tasks.length + 1;
+    }
+    const toAdd = { index: length, completed: false, desc: input.value };
+    localStorage.setItem('ITEMS', JSON.stringify([...JSON.parse(localStorage.getItem('ITEMS') || '[]'), toAdd]));
+    content.appendChild(AddItem(toAdd));
+    input.value = '';
+  }
+};
+
+document.getElementById('enterButton').addEventListener('click', () => addTask());
+
+document.getElementById('itemInput').addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    addTask();
+  }
+});
+
+const clear = document.createElement('button');
+clear.setAttribute('id', 'clearbutton');
+clear.className = 'clear';
+clear.innerText = 'Clear all Completed';
+
+clear.addEventListener('click', () => {
+  const toremove = document.querySelectorAll('input:checked');
+  [...toremove].forEach((checkbox) => {
+    checkbox.parentElement.remove();
+  });
+  localStorage.clear();
+  const available = document.querySelectorAll('.desc');
+  if (available.length > 0) {
+    const newArray = [];
+
+    let length = 1;
+
+    [...available].forEach((child) => {
+      const toAdd = { index: length, completed: false, desc: child.innerText };
+      newArray.push(toAdd);
+      length += 1;
+    });
+    localStorage.setItem('ITEMS', JSON.stringify(newArray));
+  }
+});
+
+box.appendChild(clear);
---
+++ src/modules/addItem.js
@@ -0,0 +1,81 @@
+import Delete from '../assets/delete.svg';
+
+const removeTask = () => {
+  const toRemove = document.getElementById('delButton');
+  toRemove.parentElement.remove();
+
+  localStorage.clear();
+
+  const available = document.querySelectorAll('.desc');
+  if (available.length > 0) {
+    const newArray = [];
+
+    let length = 1;
+
+    [...available].forEach((child) => {
+      const toAdd = { index: length, completed: false, desc: child.innerText };
+      newArray.push(toAdd);
+      length += 1;
+    });
+    localStorage.setItem('ITEMS', JSON.stringify(newArray));
+  }
+};
+
+const updateTask = () => {
+  //
+  localStorage.getItem('ITEMS');
+  const available = document.querySelectorAll('#checkbox');
+  if (available.length > 0) {
+    const newArray = [];
+    let length = 1;
+    [...available].forEach((child) => {
+      const toAdd = {
+        index: length,
+        completed: child.checked,
+        desc: child.nextElementSibling.innerText,
+      };
+      newArray.push(toAdd);
+      length += 1;
+    });
+    localStorage.setItem('ITEMS', JSON.stringify(newArray));
+  }
+};
+
+const AddItem = (item) => {
+  const rowitem = document.createElement('div');
+  rowitem.className = 'row-item';
+
+  const boolean = document.createElement('input');
+  boolean.setAttribute('id', 'checkbox');
+  boolean.type = 'checkbox';
+  boolean.checked = item.completed;
+  if (boolean.checkd) {
+    boolean.click();
+  }
+  boolean.addEventListener('change', updateTask);
+
+  const desc = document.createElement('div');
+  desc.className = 'desc';
+  desc.innerText = item.desc;
+  desc.contentEditable = true;
+  desc.addEventListener('change', updateTask);
+
+  const delImg = document.createElement('img');
+  delImg.setAttribute('src', Delete);
+  delImg.setAttribute('width', 30);
+  delImg.setAttribute('height', 30);
+  delImg.setAttribute('alt', 'showmenu');
+
+  const delButton = document.createElement('button');
+  delButton.setAttribute('id', 'delButton');
+  delButton.appendChild(delImg);
+  delButton.addEventListener('click', removeTask);
+
+  rowitem.appendChild(boolean);
+  rowitem.appendChild(desc);
+  rowitem.appendChild(delButton);
+
+  return rowitem;
+};
+
+export default AddItem;
